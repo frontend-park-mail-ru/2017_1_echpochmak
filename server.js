99ffffffa6f4a59a06f59fec8fef576e9d35e60a
VyazminIV <ilja.vyazmin@mail.ru>
2017-02-23 15:07:48 +0300

    no work

--- server.js
+++ server.js
@@ -11,23 +11,7 @@ const worker = function (request, response) {
 	const url = request.url;
 
 	// Определяем, какой из файлов мы будем использовать
-	let content;
-	if (url === '/') {
-		content = fs.readFileSync('./static/menu.html', 'utf8');
-	} else if (url === '/login') {
-		content = fs.readFileSync('./static/login.html', 'utf8');
-	} else if (url === '/signup') {
-		content = fs.readFileSync('./static/signup.html', 'utf8');
-	} else if (url === '/leaderboard') {
-		content = fs.readFileSync('./static/leaderboard.html', 'utf8');
-	} else if (url === '/about') {
-		content = fs.readFileSync('./static/about.html', 'utf8');
-	} else if (url === '/game') {
-		content = fs.readFileSync('./static/game.html', 'utf8');
-	} else {
-		content = fs.readFileSync('./static/menu.html', 'utf8');
-	}
-
+	let content = fs.readFileSync('./static/menu.html', 'utf8');
 	// Записываем заголовок в ответ
 	response.writeHead(200, {"Content-Type": "text/html"});
 

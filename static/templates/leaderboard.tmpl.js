b5a4b08b99d548896d20acbe253f79f0c8f3a42b
yaches <@>
2017-03-10 07:23:43 +0300

    Шаблоны и почти полная интеграция

---
+++ static/templates/leaderboard.tmpl.js
@@ -0,0 +1,33 @@
+;(function(){var x=Function('return this')();if(!x.fest)x.fest={};x.fest['leaderboard.tmpl']=function (__fest_context){"use strict";var __fest_self=this,__fest_buf="",__fest_chunks=[],__fest_chunk,__fest_attrs=[],__fest_select,__fest_if,__fest_iterator,__fest_to,__fest_fn,__fest_html="",__fest_blocks={},__fest_params,__fest_element,__fest_debug_file="",__fest_debug_line="",__fest_debug_block="",__fest_element_stack = [],__fest_short_tags = {"area": true, "base": true, "br": true, "col": true, "command": true, "embed": true, "hr": true, "img": true, "input": true, "keygen": true, "link": true, "meta": true, "param": true, "source": true, "wbr": true},__fest_jschars = /[\\'"\/\n\r\t\b\f<>]/g,__fest_jschars_test = /[\\'"\/\n\r\t\b\f<>]/,__fest_htmlchars = /[&<>"]/g,__fest_htmlchars_test = /[&<>"]/,__fest_jshash = {"\"": "\\\"", "\\": "\\\\", "/": "\\/", "\n": "\\n", "\r": "\\r", "\t": "\\t", "\b": "\\b", "\f": "\\f", "'": "\\'", "<": "\\u003C", ">": "\\u003E"},__fest_htmlhash = {"&": "&amp;", "<": "&lt;", ">": "&gt;", "\"": "&quot;"},__fest_escapeJS = function __fest_escapeJS(value) {
+		if (typeof value === 'string') {
+			if (__fest_jschars_test.test(value)) {
+				return value.replace(__fest_jschars, __fest_replaceJS);
+			}
+		}
+
+		return value == null ? '' : value;
+	},__fest_replaceJS = function __fest_replaceJS(chr) {
+		return __fest_jshash[chr];
+	},__fest_escapeHTML = function __fest_escapeHTML(value) {
+		if (typeof value === 'string') {
+			if (__fest_htmlchars_test.test(value)) {
+				return value.replace(__fest_htmlchars, __fest_replaceHTML);
+			}
+		}
+
+		return value == null ? '' : value;
+	},__fest_replaceHTML = function __fest_replaceHTML(chr) {
+		return __fest_htmlhash[chr];
+	},__fest_extend = function __fest_extend(dest, src) {
+		for (var key in src) {
+			if (src.hasOwnProperty(key)) {
+				dest[key] = src[key];
+			}
+		}
+	},__fest_param = function __fest_param(fn) {
+		fn.param = true;
+		return fn;
+	},i18n=__fest_self && typeof __fest_self.i18n === "function" ? __fest_self.i18n : function (str) {return str;},___fest_log_error;if(typeof __fest_error === "undefined"){___fest_log_error = (typeof console !== "undefined" && console.error) ? function(){return Function.prototype.apply.call(console.error, console, arguments)} : function(){};}else{___fest_log_error=__fest_error};function __fest_log_error(msg){___fest_log_error(msg+"\nin block \""+__fest_debug_block+"\" at line: "+__fest_debug_line+"\nfile: "+__fest_debug_file)}function __fest_call(fn, params,cp){if(cp)for(var i in params)if(typeof params[i]=="function"&&params[i].param)params[i]=params[i]();return fn.call(__fest_self,params)}var json=__fest_context;(function(__fest_context){var json=__fest_context;__fest_blocks.record=function(params){var __fest_buf="";__fest_buf+=("<div class=\"line row\"><div class=\"col-xs-3 col-sm-3 col-md-3 col-lg-3\" align=\"center\">");try{__fest_buf+=(params.rating + 1)}catch(e){__fest_log_error(e.message + "6");}__fest_buf+=("</div><div class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6\" align=\"center\">");try{__fest_buf+=(params.user.login)}catch(e){__fest_log_error(e.message + "9");}__fest_buf+=("</div><div class=\"col-xs-3 col-sm-3 col-md-3 col-lg-3\" align=\"center\">");try{__fest_buf+=(params.user.score)}catch(e){__fest_log_error(e.message + "12");}__fest_buf+=("</div></div>");return __fest_buf;};})(__fest_context);__fest_buf+=("<div class=\"list-header\" align=\"center\">Доска героев<hr/></div><div class=\"line row list-sub-header\"><div class=\"col-xs-3 col-sm-3 col-md-3 col-lg-3\" align=\"center\">Рейтинг</div><div class=\"col-xs-6 col-sm-6 col-md-6 col-lg-6\" align=\"center\">Имя</div><div class=\"col-xs-3 col-sm-3 col-md-3 col-lg-3\" align=\"center\">Результат</div></div>");var i,__fest_to0,__fest_iterator0;try{__fest_iterator0=json.users || [];__fest_to0=__fest_iterator0.length;}catch(e){__fest_iterator0=[];__fest_to0=0;__fest_log_error(e.message);}for(i=0;i<__fest_to0;i++){__fest_select="record";__fest_params={};try{__fest_extend(__fest_params,{
+					user: json.users[i],
+					rating: i
+				})}catch(e){__fest_log_error(e.message)}__fest_chunks.push(__fest_buf,{name:__fest_select,params:__fest_params,cp:false});__fest_buf="";}__fest_to=__fest_chunks.length;if (__fest_to) {__fest_iterator = 0;for (;__fest_iterator<__fest_to;__fest_iterator++) {__fest_chunk=__fest_chunks[__fest_iterator];if (typeof __fest_chunk==="string") {__fest_html+=__fest_chunk;} else {__fest_fn=__fest_blocks[__fest_chunk.name];if (__fest_fn) __fest_html+=__fest_call(__fest_fn,__fest_chunk.params,__fest_chunk.cp);}}return __fest_html+__fest_buf;} else {return __fest_buf;}}})();
--- static/views/leaderboard.js
+++ static/views/leaderboard.js
@@ -9,41 +9,27 @@
 			});
 			this.list = new BaseBlock('div', {
 				'class': 'list'
-			});
-			this.header = new BaseBlock('div', {
-				'class': 'list-header', 
-				'align': 'center'
-			});
-			this.header.get().innerHTML = 'Лучшие игроки';
-			this.subheader = new BaseBlock('div', {
-				'class': 'line row list-sub-header'
 			})
-			this.rating = new BaseBlock('div', {
-				'class': 'col-xs-3 col-sm-3 col-md-3 col-lg-3',
-				'align': 'center'
-			});
-			this.name = new BaseBlock('div', {
-				'class': 'col-xs-6 col-sm-6 col-md-6 col-lg-6',
-				'align': 'center'
-			});
-			this.score = new BaseBlock('div', {
-				'class': 'col-xs-3 col-sm-3 col-md-3 col-lg-3',
-				'align': 'center'
-			});
-			this.rating.get().innerHTML = 'Рейтинг';
-			this.name.get().innerHTML = 'Имя';
-			this.score.get().innerHTML = 'Результат';
-
 			this.render();
 		}
 
 		render() {
 			this.get().appendChild(this.list.get());
-			this.list.get().appendChild(this.header.get());
-			this.list.get().appendChild(this.subheader.get());
-			this.subheader.get().appendChild(this.rating.get());
-			this.subheader.get().appendChild(this.name.get());
-			this.subheader.get().appendChild(this.score.get());
+
+			this.template = window.fest['leaderboard.tmpl'];
+			this.list.get().innerHTML = this.template({});
+		}
+
+		show() {
+			document.querySelector('body').appendChild(this.get());
+			this.update();
+		}
+
+		update() {
+			const service = new UserService();
+			service.getUsersList(xhr => {
+				this.list.get().innerHTML = this.template(xhr);
+			})
 		}
 	}
 
